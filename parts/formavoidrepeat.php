05eefee671e68049b33625b78f9cfc62d183e059
rachid <bobirachid@gmail.com>
2022-03-11 21:49:25 +0100

    filter form in index

---
+++ parts/formavoidrepeat.php
@@ -0,0 +1,10 @@
+<form action='filter.php' method="get">
+    <label for="cat">category</label>
+    <select id='cat' name="category">
+    <option value="samsung">samsung</option>
+    <option value="iphone">iphone</option>
+    <option value="huawei">huawei</option>
+    </select>
+<button class="btn btn-outline-primary" type="submit">filter</button>
+
+    </form>
