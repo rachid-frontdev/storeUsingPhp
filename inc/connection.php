bb627b0ce04bf265518238171d9e1d6f1c4b7eea
rachid <bobirachid@gmail.com>
2022-03-16 21:27:31 +0100

    setup db connection to remote SQL

--- inc/connection.php
+++ inc/connection.php
@@ -1,5 +1,13 @@
 <?php 
-$connection = mysqli_connect("localhost","root","","basefirst");
+//$connection = mysqli_connect("localhost","root","","basefirst");
+$port =3306;
+$connection = mysqli_connect("sql11.freesqldatabase.com","sql11479518","3b8b1Mtxe6","sql11479518", $port);
+//$Host: sql11.freesqldatabase.com
+//$Database name: sql11479518
+//Database user: sql11479518
+//Database password: 3b8b1Mtxe6
+
+
 if(!$connection) {
     echo 'db failed to match!';
 }
