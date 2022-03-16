d32119d9eeaba22721e03af9af69f11fc0ca7841
rachid <bobirachid@gmail.com>
2022-03-16 21:52:21 +0100

    change Generic Database Assignment

--- inc/connection.php
+++ inc/connection.php
@@ -1,12 +1,6 @@
 <?php 
 //$connection = mysqli_connect("localhost","root","","basefirst");
-$port =3306;
-$connection = mysqli_connect("sql11.freesqldatabase.com","sql11479518","3b8b1Mtxe6","sql11479518", $port);
-//$Host: sql11.freesqldatabase.com
-//$Database name: sql11479518
-//Database user: sql11479518
-//Database password: 3b8b1Mtxe6
-
+$connection = mysqli_connect("sql11.freesqldatabase.com","sql11479518","3b8b1Mtxe6","sql11479518", 3306);
 
 if(!$connection) {
     echo 'db failed to match!';
